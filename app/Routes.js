59feca93219287d294abede2514ebd67ae4d08c4
Ryan Rogalski <ryan@infiniteloopco.com>
2017-07-28 15:43:23 -0700

    enable hmr by making Routes not stateless comp

--- app/Routes.js
+++ app/Routes.js
@@ -5,14 +5,15 @@ import Home from './comp/Home'
 import About from './comp/About'
 import style from './comp/variables.css'
 
-const Routes = () => (
-  <Router>
-    <Layout>
-      <Route exact path="/" component={ Home } />
-      <Route path="/about" component={ About } />        
-    </Layout>
-  </Router>
-)
-
-export default Routes
-
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Layout>
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About } />        
+        </Layout>
+      </Router>
+    )
+  }
+} 
