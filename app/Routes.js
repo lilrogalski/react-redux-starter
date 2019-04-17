08f1684121d6d67dd9d02865b3efa12bf7d1bc8c
Ryan Rogalski <ryan@infiniteloopco.com>
2019-04-17 10:58:24 -0700

    add playground

--- app/Routes.js
+++ app/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Layout from './comp/Layout'
 import Home from './comp/Home'
 import About from './comp/About'
+import Playground from './comp/Playground'
 
 // use a class to ensure HMR works properly, this would normally be a stateless component
 // eslint-disable-next-line react/prefer-stateless-function
@@ -11,8 +12,9 @@ export default class Routes extends React.Component {
     return (
       <Router>
         <Layout>
-          <Route exact path="/" component={ Home } />
-          <Route path="/about" component={ About } />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/playground" component={Playground} />
         </Layout>
       </Router>
     )
---
+++ app/comp/Playground/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './style.css'
+
+export default function Playground () {
+  return (
+    <div className={style.playground}>
+      playground
+    </div>
+  )
+}
